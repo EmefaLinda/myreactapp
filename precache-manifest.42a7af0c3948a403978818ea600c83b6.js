631fa19c20ec1e18f60e8cf8f9b884b08c67abe8
EmefaLinda <lindaemf@gmail.com>
2020-08-12 08:34:16 -0700

    Updates

---
+++ precache-manifest.42a7af0c3948a403978818ea600c83b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee80102822b4b8653466e8157045decb",
+    "url": "/myreactapp/index.html"
+  },
+  {
+    "revision": "0cd9ad8c6d0084e8e9de",
+    "url": "/myreactapp/static/css/main.78fa1dd3.chunk.css"
+  },
+  {
+    "revision": "129dc17a5679e0d0d8d4",
+    "url": "/myreactapp/static/js/2.3addd467.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreactapp/static/js/2.3addd467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd9ad8c6d0084e8e9de",
+    "url": "/myreactapp/static/js/main.758261d9.chunk.js"
+  },
+  {
+    "revision": "5abcf5dd1171be5f087d",
+    "url": "/myreactapp/static/js/runtime-main.944d8c5b.js"
+  },
+  {
+    "revision": "6f86394eb05c392f63b42e3666af9a11",
+    "url": "/myreactapp/static/media/47386501_974804009384783_6028323879422787584_n.6f86394e.jpg"
+  },
+  {
+    "revision": "cb88bc94e3a297f8b2158977d666e3ea",
+    "url": "/myreactapp/static/media/81649500_1243309449200903_816664469143814144_n.cb88bc94.jpg"
+  }
+]);
